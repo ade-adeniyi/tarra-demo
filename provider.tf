10e8850d9960eb1a6b54c8fe3a523b4999d3349a
agboke <64755022+agboke@users.noreply.github.com>
2022-07-25 19:52:44 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,4 @@
 provider "aws" {
   region                  = "us-east-1"
-  access_key              = "AKIAUW25LE7MDXZ62RJJ"
-  secret_key              = "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"
+  profile                 = "myaws"
 }
