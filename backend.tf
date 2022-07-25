f111c54aa1ce4e1e4001ad7bddaf760d505d4c4a
agboke <64755022+agboke@users.noreply.github.com>
2022-07-25 16:48:47 -0400

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket = "dev-tf-state-file"
+    key = "main-key"
+    region = "us-east-1"
+    profile = "default"
+    dynamodb_table = "my-dyna-table"
+  }
+}
