7815c2392d4318f4e7c9d7a5619b4cb3f9e6274d
ade-adeniyi <121461784+ade-adeniyi@users.noreply.github.com>
2023-04-30 21:26:22 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -7,3 +7,11 @@
       #dynamodb_table  = "my-dyna-table"
   #}
 #}
+terraform {
+  backend "s3" {
+    bucket = "my-aws-tf-state-demo"
+    key = "main"
+    region = "us-east-2"
+    dynamodb_table = "my-dynamo-db-table"
+  }
+}
